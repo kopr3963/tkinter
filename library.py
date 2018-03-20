48d93d4da98bb7af4d08d0a603dc4b74f739f6e3
kopr3963 <kopr3963@naver.com>
2018-03-20 20:23:39 +0900

    library add

---
+++ library.py
@@ -0,0 +1,50 @@
+#library.py
+from Tkinter import Menu
+print('a');
+
+
+def __init__():
+	print('init');
+
+def test() : 
+	print('test');
+
+def hello():
+	print ("hi");
+
+
+
+def settings_Menu(self):
+	print('library.py  settings_menu()' );
+	self.menubar = Menu(self);
+	self.menubar.add_command(label="Hello!", command=hello)
+	self.menubar.add_command(label="Quit!", command=self.quit)
+
+	filemenu = Menu(self.menubar, tearoff=0)
+	filemenu.add_command(label="Open", command=hello)
+	filemenu.add_command(label="Save", command=hello)
+	filemenu.add_separator()
+	filemenu.add_command(label="Exit", command=self.quit)
+	self.menubar.add_cascade(label="File", menu=filemenu)
+
+
+
+	editmenu = Menu(self.menubar, tearoff=0)
+	editmenu.add_command(label="Cut", command=hello)
+	editmenu.add_command(label="Copy", command=hello)
+	editmenu.add_command(label="Paste", command=hello)
+	self.menubar.add_cascade(label="Edit", menu=editmenu)
+
+	helpmenu = Menu(self.menubar, tearoff=0)
+	helpmenu.add_command(label="About", command=hello)
+	self.menubar.add_cascade(label="Help", menu=helpmenu)
+
+	self.config(menu=self.menubar)
+
+
+	pass;
+
+
+
+	
+	
